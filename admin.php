2920c12fa21818e560f9770b96368214ad78c23a
SerafimE <38590113+SerafimE@users.noreply.github.com>
2018-07-14 18:12:33 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,17 +1,21 @@
-<?php
+<?php declare(strict_types=1);
+
 if (isset($_POST) && isset($_FILES) && isset($_FILES['testFile'])) {
-    $fileName = $_FILES['testFile']['name'];
+    $oldFileName = $_FILES['testFile']['name'];
     $tmpFile = $_FILES['testFile']['tmp_name'];
-    $uploadsDir = 'uploads/';
-    $pathInfo = pathinfo($uploadsDir . $fileName);
+    $uploadsDir = 'upload/';
+    $location = './list.php';
+    $pathInfo = pathinfo($uploadsDir . $oldFileName);
+    $fileName = md5($pathInfo['filename']) . '.' . $pathInfo['extension'];
     if ($pathInfo['extension'] === 'json') {
         move_uploaded_file($tmpFile, $uploadsDir . $fileName);
-        echo 'Спасибо, Ваш тест загружен!';
+        header("Location: $location");
     } else {
-        echo 'Извините, нужен файл с расширением .json';
+        echo '<h2>Извините, нужен файл с расширением .json</h2>';
     }
 }
 ?>
+
 <!doctype html>
 <html lang="ru">
 <head>
@@ -24,10 +28,37 @@ if (isset($_POST) && isset($_FILES) && isset($_FILES['testFile'])) {
     <input type=file name="testFile">
     <input type=submit value=Загрузить>
 </form>
-
 <ul>
     <li><a href="admin.php">Загрузить тест</a></li>
     <li><a href="list.php">Список тестов</a></li>
 </ul>
+<h2>После загрузки файла .json Вы будете перенаправлены на страницу со списком тестов.</h2>
+<h3>Образец файла .json</h3>
+<p>Количество блоков с вопросами может быть любым, количество вариантов ответов так же, но в разумных пределах,
+    правильный ответ конечно один на блок.</p>
+<pre>
+[
+  {
+    "question": "Кто является создателем сети vk.com?",
+    "answers": [
+      "Юрий Гагарин",
+      "Павел Дуров",
+      "Марк Цукерберг",
+      "Сергей Люляев"
+    ],
+    "correctAnswer": "Павел Дуров"
+  },
+  {
+    "question": "Сколько будет 10*10?",
+    "answers": [
+      "100",
+      "1000",
+      "10",
+      "85"
+    ],
+    "correctAnswer": "100"
+  }
+]
+</pre>
 </body>
 </html>
